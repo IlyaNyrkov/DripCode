074d10e377cdb1003ed1963582aa03dd3c84d747
ilya <ilya.nyrkov@gmail.com>
2022-01-24 03:18:43 +0300

    moved gitignore from twosumfolder | added solution of longest substring without repeating chars

---
+++ longestSubstrWthtRepChars/longestSubstrWthtRepChars.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // array of all ASCII character positions in given string
+        int arr[127];
+        for (int i = 0; i < 127; ++i) {
+            arr[i] = -1;
+        }
+        // start of string
+        int start = 0;
+        // max character count in string
+        int ans = 0;
+        // current character count in string
+        int counter = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            int charIndex = s[i] - ' ';
+            if (arr[charIndex] < start) {
+                ans = max(ans, ++counter);
+            } else {
+                // if we find reoccuring of character we move starting point by one
+                start = arr[charIndex] + 1;
+                // since we moved starting point we need to change counter value
+                // we substract indexes of second appearence of a character and first appearence
+                // that way we can keep on moving with pointer i and not loosing counter
+                //
+                //  s,i             s  i                s  i
+                //  |           ->  |  | counter = 3 ->  |  | counter = 3
+                //  abcadcbba       abcadcbba          abcadcbba
+                counter = i - arr[charIndex];
+            }
+            arr[charIndex] = i;
+        }
+        return ans;
+    }
+};
+
+int main() {
+
+    return 0;
+}
