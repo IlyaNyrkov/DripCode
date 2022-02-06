547fa70903ce59690556661a0b527e904e59b978
ilya <ilya.nyrkov@gmail.com>
2022-02-06 21:43:36 +0300

    added second solution variant for intersectonOfTwoArrays

--- intersectionOfTwoArrays/intersectionOfTwoArrays.cpp
+++ intersectionOfTwoArrays/intersectionOfTwoArrays.cpp
@@ -20,6 +20,26 @@ public:
     }
 };
 
+// second variant of solution
+class Solution2 {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        bool occurances[1001] = {false};
+        vector<int> result;
+        for (size_t i = 0; i < nums1.size(); ++i) {
+            occurances[nums1[i]] = true;
+        }
+        for (size_t i = 0; i < nums2.size(); ++i) {
+            if (occurances[nums2[i]]) {
+                occurances[nums2[i]] = false;
+                result.push_back(nums2[i]);
+            }
+        }
+        return result;
+    }
+};
+
+
 bool testSolution(vector<int> nums1, vector<int> nums2, vector<int> expected) {
     Solution solution;
     return solution.intersection(nums1, nums2) == expected;
